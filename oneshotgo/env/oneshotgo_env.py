27deb8eeeab9b54f5a22da007fef5935894e9916
Kurt <callmekoo@naver.com>
2019-01-07 00:35:21 +0900

    add comment

--- oneshotgo/env/oneshotgo_env.py
+++ oneshotgo/env/oneshotgo_env.py
@@ -10,23 +10,40 @@ from PIL import Image
 from os.path import abspath, join, dirname
 
 """
-0 - No guess yet submitted (only after reset)
+***************
+* observation *
+***************
+0 - No guess yet
 1 - Guess is lower than the target
 2 - Guess is equal to the target
 3 - Guess is higher than the target
+
+*************
+* Data file *
+*************
+012.bmp : Blood cell 10x10 Image
+012.png : Blood cell 10x10 mask
+
+012Ori.bmp : Blood cell 300x300 Image (full size)
+012Ori.png : Blood cell 300x300 mask (full size)
+
+065.bmp : Blood cell 100x100 Image
+065.png : Blood cell 100x100 mask
+
+065Ori.bmp : Blood cell 300x300 Image (full size)
+065Ori.png : Blood cell 300x300 mask (full size)
 """
+TRAIN_IMAGE = "012.bmp"
+TRAIN_MASK = "012.png"
+
 class OneShotGoEnv(gym.Env):
     def __init__(self):
         seed()
         self.seed()
 
         script_dir = dirname(__file__)
-        self.img = Image.open(abspath(join(script_dir, "..", "data", "012.bmp")))
-        #self.img = Image.open(r"E:\gym\gym\envs\pap\065.bmp")
-        #self.img = Image.open(r"E:\gym\gym\envs\pap\012Ori.bmp")
-        self.mask = np.asarray(Image.open(abspath(join(script_dir, "..", "data", "012.png"))))
-        #self.mask = np.asarray(Image.open(r"E:\gym\gym\envs\pap\065.png"))
-        #self.mask = np.asarray(Image.open(r"E:\gym\gym\envs\pap\012Ori.png"))
+        self.img = Image.open(abspath(join(script_dir, "..", "data", TRAIN_IMAGE)))
+        self.mask = np.asarray(Image.open(abspath(join(script_dir, "..", "data", TRAIN_MASK))))
         self.mask_zero_count = np.count_nonzero(self.mask[...,0]==0)
 
         self.width, self.height = self.img.size
